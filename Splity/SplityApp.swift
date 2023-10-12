333da116133ba25712eb3c876da34df35483c47c
Horacio Mota <horaciomota93u@gmail.com>
2023-10-12 03:43:18 -0700

    Initial Commit

---
+++ Splity/SplityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplityApp.swift
+//  Splity
+//
+//  Created by Horacio Mota on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SplityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
